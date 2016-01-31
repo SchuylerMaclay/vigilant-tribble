7e2e358a8038b7a49c591a93f79af148a246a432
SchuylerMaclay <schuylermaclay@gmail.com>
2016-01-31 12:00:01 -0500

    zip code lookup working

--- app/assets/elm/hello.elm
+++ app/assets/elm/hello.elm
@@ -12,27 +12,97 @@ import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Mouse
 import Window
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
+import Keyboard
+import Text
+import Time exposing (..)
+import Window
+
+import Char
+import Html exposing (..)
+import Html.Attributes as Attr exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Json exposing ((:=))
+import String
+import Task exposing (..)
 
 port noun : String
 
+-- VIEW
 
+view : String -> Result String (List String) -> Html
+view string result =
+  let field =
+        input
+          [ placeholder "Zip Code"
+          , value string
+          , on "input" targetValue (Signal.message query.address)
+          , myStyle
+          ]
+          []
 
-main : Signal Element
-main =
-  Signal.map2 scene Mouse.position Window.dimensions
+      messages =
+        case result of
+          Err msg ->
+              [ div [ myStyle ] [ Html.text msg ] ]
 
-
-scene : (Int,Int) -> (Int,Int) -> Element
-scene (x,y) (w,h) =
-  let
-    (dx,dy) =
-      (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
+          Ok cities ->
+              List.map (\city -> div [ myStyle ] [ Html.text city ]) cities
   in
-    collage w h
-      [ ngon 3 100
-          |> filled blue
-          |> rotate (atan2 dy dx)
-      , ngon 6 30
-          |> filled orange
-          |> move (dx, dy)
-      ]
+      div [] (field :: messages)
+
+
+myStyle : Attribute
+myStyle =
+  style
+    [ ("width", "100%")
+    , ("height", "40px")
+    , ("padding", "10px 0")
+    , ("font-size", "2em")
+    , ("text-align", "center")
+    ]
+
+
+-- WIRING
+
+main =
+  Signal.map2 view query.signal results.signal
+
+
+query : Signal.Mailbox String
+query =
+  Signal.mailbox ""
+
+
+results : Signal.Mailbox (Result String (List String))
+results =
+  Signal.mailbox (Err "A valid US zip code is 5 numbers.")
+
+
+port requests : Signal (Task x ())
+port requests =
+  Signal.map lookupZipCode query.signal
+    |> Signal.map (\task -> Task.toResult task `andThen` Signal.send results.address)
+
+
+lookupZipCode : String -> Task String (List String)
+lookupZipCode query =
+  let toUrl =
+        if String.length query == 5 && String.all Char.isDigit query
+          then succeed ("http://api.zippopotam.us/us/" ++ query)
+          else fail "Give me a valid US zip code!"
+  in
+      toUrl `andThen` (mapError (always "Not found :(") << Http.get places)
+
+
+places : Json.Decoder (List String)
+places =
+  let place =
+        Json.object2 (\city state -> city ++ ", " ++ state)
+          ("place name" := Json.string)
+          ("state" := Json.string)
+  in
+      "places" := Json.list place
